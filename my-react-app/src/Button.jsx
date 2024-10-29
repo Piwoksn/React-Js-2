e101971fa5453c9adf57dc485d264f3ec676717b
Piwoksn <piwoksn@gmail.com>
2024-10-29 10:06:24 +0100

    using inline css in react is like using dictionaries

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,5 +1,13 @@
 function Button() {
-  return <button className={styles.button}>Click Me</button>;
+  const styles = {
+    backgroundColor: "hsl(200, 100%, 50%)",
+    color: "white",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+  };
+  return <button style={styles}>Click Me</button>;
 }
 
 export default Button;
