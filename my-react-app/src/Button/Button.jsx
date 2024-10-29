ae88e95e65f834b7e0941175bdf8ef64977672a5
Piwoksn <piwoksn@gmail.com>
2024-10-29 09:59:43 +0100

    just created a button module css which am about to delete and move the Button component back to src dir

--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -1,5 +1,7 @@
+import styles from "./Button.module.css";
+
 function Button() {
-  return <button className="button">Click Me</button>;
+  return <button className={styles.button}>Click Me</button>;
 }
 
 export default Button;
