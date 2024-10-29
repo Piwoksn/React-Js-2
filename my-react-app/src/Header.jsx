7bffaf5d3d1f65392a977ece853237112bf79779
Piwoksn <piwoksn@gmail.com>
2024-10-29 07:43:12 +0100

    updating

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,6 +2,20 @@ function Header() {
   return (
     <header>
       <h1>My Header</h1>
+      <ul>
+        <li>
+          <a href="http:#">Home</a>
+        </li>
+        <li>
+          <a href="http:#">About</a>
+        </li>
+        <li>
+          <a href="http:#">Services</a>
+        </li>
+        <li>
+          <a href="http:#">Contact</a>
+        </li>
+      </ul>
     </header>
   );
 }
