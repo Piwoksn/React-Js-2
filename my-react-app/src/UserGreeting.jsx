c48d035b8ef7f8893de400f973b9e53a4b23f2a1
Piwoksn <piwoksn@gmail.com>
2024-10-29 21:27:54 +0100

    Another way to use conditionals

--- my-react-app/src/UserGreeting.jsx
+++ my-react-app/src/UserGreeting.jsx
@@ -1,9 +1,16 @@
 function UserGreeting(props) {
-  if (props.isLoggedIn) {
-    return <h2>Welcome {props.username}</h2>;
-  } else {
-    return <h2>Please login to continue</h2>;
-  }
+  //   if (props.isLoggedIn) {
+  //     return <h2>Welcome {props.username}</h2>;
+  //   } else {
+  //     return <h2>Please login to continue</h2>;
+  //   }
+  // Or do this way
+
+  return props.isLoggedIn ? (
+    <h2>Welcome {props.username}</h2>
+  ) : (
+    <h2>Please login to continue</h2>
+  );
 }
 
 export default UserGreeting;
