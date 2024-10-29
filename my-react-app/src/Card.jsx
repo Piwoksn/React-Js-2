d09173ad49db76796f1fd9e3f395e68d0913c8dd
Piwoksn <piwoksn@gmail.com>
2024-10-29 09:18:23 +0100

    finished styling my card component

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -2,9 +2,11 @@ import profilepic from "./assets/my_img/img.png";
 function Card() {
   return (
     <div className="card">
-      <img src={profilepic} alt="profile" />
-      <h2>Mccoy Noble</h2>
-      <p>I am a tech enthusiast. One who seeks knowledge to become great</p>
+      <img className="card-image" src={profilepic} alt="profile" />
+      <h2 className="card-title">Mccoy Noble</h2>
+      <p className="card-text">
+        I am a tech enthusiast. One who seeks knowledge to become great
+      </p>
     </div>
   );
 }
