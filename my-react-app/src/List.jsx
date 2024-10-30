2ae3ea6058b602a70acd0dafc763f6e0b8e6d3f9
Piwoksn <piwoksn@gmail.com>
2024-10-30 10:51:12 +0100

    You can also do this

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,14 +1,17 @@
 function List() {
   const fruits = ["Orange", "Apple", "Pineapple", "Coconut", "Banana"];
 
-  const listItem = fruits.map((item, index) => {
-    return (
-      <ul>
-        <li key={index}>{item}</li>
-      </ul>
-    );
-  });
-  return listItem;
+  // const listItem = fruits.map((item, index) => {
+  //   return (
+  //     <ul>
+  //       <li key={index}>{item}</li>
+  //     </ul>
+  //   );
+  // });
+
+  // or this
+  const listItem = fruits.map((item) => <li>{item}</li>);
+  return <ul> {listItem}</ul>;
 }
 
 export default List;
