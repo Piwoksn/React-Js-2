5f8afdb5faace1b760ada5c53b382182949dac1c
Piwoksn <piwoksn@gmail.com>
2024-10-30 13:04:08 +0100

    created functions to handle event click

--- my-react-app/src/Button2.jsx
+++ my-react-app/src/Button2.jsx
@@ -1,9 +1,36 @@
 function Button2() {
+  const styles = {
+    marginTop: "10px",
+    backgroundColor: "red",
+    padding: "5px 10px",
+    color: "white",
+    borderRadius: "5px",
+    border: "none",
+    fontSize: "16px",
+  };
   const handleClick = () => console.log("OUCH!");
 
   const handleClick2 = (name) => console.log(`${name} stop clicking me!`);
 
-  return <button onClick={() => handleClick2("Mccoy")}>Click Me :)</button>;
+  let count = 0;
+  function handleClick3(name) {
+    if (count < 3) {
+      count++;
+      console.log(`${name} you clicked me ${count} time/s`);
+    } else {
+      console.log(`${name} stop clicking me!`);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={() => handleClick3("Mccoy")}>Click Me :)</button>
+      <br />
+      <button style={styles} onClick={() => alert("You Just Clicked me.")}>
+        Alert
+      </button>
+    </>
+  );
 }
 
 export default Button2;
