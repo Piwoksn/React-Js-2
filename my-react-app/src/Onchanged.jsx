ec900f7170086ed9f756aefe4a0f0a72775c200c
Piwoksn <piwoksn@gmail.com>
2024-10-30 17:00:46 +0100

    created a comment section

--- my-react-app/src/Onchanged.jsx
+++ my-react-app/src/Onchanged.jsx
@@ -12,6 +12,11 @@ function OnChangeEvent() {
   const handleQtyChange = (e) => setQty(e.target.value);
 
   // textarea
+  const [comment, setComment] = useState("");
+
+  function commentHandler(e) {
+    setComment(e.target.value);
+  }
 
   return (
     <div>
@@ -19,6 +24,14 @@ function OnChangeEvent() {
       <h3>Name: {name}</h3>
       <input type="number" value={qty} onChange={handleQtyChange} />
       <h3>Name: {qty}</h3>
+
+      <textarea
+        value={comment}
+        onChange={commentHandler}
+        placeholder="Enter Comment"
+      ></textarea>
+
+      <p>{comment}</p>
     </div>
   );
 }
