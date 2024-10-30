7216fd7a7d6b1bab9c99c14f112dfb19a8a6c116
Piwoksn <piwoksn@gmail.com>
2024-10-30 11:30:52 +0100

    filtered out the ones greater than 100

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -23,11 +23,17 @@ function List() {
   // fruits.sort((a, b) => a.calories - b.calories); //sorts the dictionary by calories
   fruits.sort((a, b) => b.calories - a.calories); //sorts the dictionary by Reversed calories
 
-  const listItem = fruits.map((fruit, index) => (
+  // creating an array with low calories
+
+  const lowCalFruit = fruits.filter((fruit) => fruit.calories < 100);
+
+  //
+  const listItem = lowCalFruit.map((lowCalFruit, index) => (
     <li key={index}>
-      {fruit.name}: &nbsp;<b>{fruit.calories}</b>
+      {lowCalFruit.name}: &nbsp;<b>{lowCalFruit.calories}</b>
     </li>
   ));
+  // return <ol> {listItem}</ol>;
   return <ol> {listItem}</ol>;
 }
 
