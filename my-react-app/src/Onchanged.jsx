95db51024cab168a2b338971597bae3d8b72ee35
Piwoksn <piwoksn@gmail.com>
2024-10-30 16:55:19 +0100

    created that of number next creating for textarea

--- my-react-app/src/Onchanged.jsx
+++ my-react-app/src/Onchanged.jsx
@@ -6,11 +6,19 @@ function OnChangeEvent() {
   const setEventHandler = (event) => {
     setName(event.target.value);
   };
+  // for Number
+  const [qty, setQty] = useState(0);
+
+  const handleQtyChange = (e) => setQty(e.target.value);
+
+  // textarea
 
   return (
     <div>
       <input type="text" value={name} onChange={setEventHandler} />
       <h3>Name: {name}</h3>
+      <input type="number" value={qty} onChange={handleQtyChange} />
+      <h3>Name: {qty}</h3>
     </div>
   );
 }
