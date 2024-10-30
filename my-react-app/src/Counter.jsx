72dfbcdd2add2cd2a135f6ea7e00527b93dc7f72
Piwoksn <piwoksn@gmail.com>
2024-10-30 14:59:44 +0100

    basic counter using the usestate function

--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 
-function Counter() {
-  let [name, setName] = useState();
-  const updateName = () => {
-    setName("McCoy");
+function Count() {
+  const [counter, setCounter] = useState(0);
+
+  const decrease = () => {
+    setCounter(counter - 1);
+  };
+  const reset = () => {
+    setCounter(0);
+  };
+  const increase = () => {
+    setCounter(counter + 1);
   };
   return (
-    <div>
-      <p>Name: {name}</p>
-      <button onClick={updateName}>Set Name</button>
-    </div>
+    <>
+      <h1>{counter}</h1>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={increase}>Increase</button>
+    </>
   );
 }
 
-export default Counter;
+export default Count;
