3f568369c8af535fb476b69542a1eb4c9a0316f5
Piwoksn <piwoksn@gmail.com>
2024-10-30 12:29:51 +0100

    created a default value and proptypes for ensure the datas are correctly entered

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 function List(props) {
   const category = props.category;
 
@@ -43,8 +44,18 @@ function List(props) {
     </>
   );
 }
-
+List.propTypes = {
+  category: PropTypes.string,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      calories: PropTypes.number,
+    })
+  ),
+};
 List.defaultProps = {
   category: "Category",
+  items: [],
 };
 export default List;
