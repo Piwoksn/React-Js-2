ee5d30bd73a89039ef31e6559966d6322e72985b
Piwoksn <piwoksn@gmail.com>
2024-10-30 10:48:59 +0100

    You can do this method

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import Card from "./Card";
 import Button from "./Button";
 import Student from "./Student";
 import UserGreeting from "./UserGreeting";
+import List from "./List";
 
 function App() {
   return (
@@ -13,7 +14,6 @@ function App() {
 
       <Header />
       <List />
-
       <UserGreeting isLoggedIn={true} username="mccoy@gmail.com" />
       <UserGreeting />
 
--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,5 +1,14 @@
 function List() {
-  return <h1>Hello</h1>;
+  const fruits = ["Orange", "Apple", "Pineapple", "Coconut", "Banana"];
+
+  const listItem = fruits.map((item, index) => {
+    return (
+      <ul>
+        <li key={index}>{item}</li>
+      </ul>
+    );
+  });
+  return listItem;
 }
 
 export default List;
