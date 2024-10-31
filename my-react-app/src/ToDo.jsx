f8398cfde50050e1d19d32d7f44df9a6c86babf6
Piwoksn <piwoksn@gmail.com>
2024-10-31 12:29:49 +0100

    down with the add function and delete function

--- my-react-app/src/ToDo.jsx
+++ my-react-app/src/ToDo.jsx
@@ -1,26 +1,42 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-  const [toDo, setToDO] = useState([
-    "Take a Walk with Rosky",
-    "Go shopping with Mum",
-    "Prepare for Exams",
-  ]);
+  const [toDo, setToDO] = useState([]);
 
   const [newToDO, setNewToDO] = useState();
+
+  const add = () => {
+    const activity = document.getElementById("new-item").value;
+    setToDO((td) => [...td, activity]);
+    document.getElementById("new-item").value = "";
+  };
+
+  const del = (index) => {
+    setToDO((td) => td.filter((element, i) => i !== index));
+  };
+
   // return
   return (
     <div className="body">
       <div className="to-to-list">
         <h1>To-Do-List</h1>
-        <input type="text" className="addInput" placeholder="Add new item" />
-        <button className="add-button">Add</button>
+        <input
+          type="text"
+          id="new-item"
+          className="addInput"
+          placeholder="Add new item"
+        />
+        <button className="add-button" onClick={add}>
+          Add
+        </button>
 
         <ol>
           {toDo.map((element, index) => (
             <li key={index}>
               <span className="text">{element}</span>
-              <button className="delete-button">Delete</button>
+              <button className="delete-button" onClick={() => del(index)}>
+                Delete
+              </button>
               <button className="move-up">Up</button>
               <button className="move-down">Down</button>
             </li>
