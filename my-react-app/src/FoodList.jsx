5c240b0e7133756f70e1b29ca6738bd5a29aad60
Piwoksn <piwoksn@gmail.com>
2024-10-31 10:43:57 +0100

    created a remove food function on the list items

--- my-react-app/src/FoodList.jsx
+++ my-react-app/src/FoodList.jsx
@@ -8,18 +8,23 @@ function FoodList() {
 
     setFood((f) => [...f, newFood]);
   };
+
+  const handleRemoveFood = (index) => {
+    setFood(foods.filter((_, i) => i !== index));
+  };
   // return
   return (
     <div>
       <ul>
         {foods.map((food, index) => (
-          <li key={index}>{food}</li>
+          <li key={index} onClick={() => handleRemoveFood(index)}>
+            {food}
+          </li>
         ))}
       </ul>
 
       <input type="text" id="foodInput" placeholder="Enter food name" />
       <button onClick={handleAddFood}>Add</button>
-      <button>Remove</button>
     </div>
   );
 }
