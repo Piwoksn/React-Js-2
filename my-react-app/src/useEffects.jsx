b0176eeec0b6663515dc18d34bcddffdbc7c6f5a
Piwoksn <piwoksn@gmail.com>
2024-10-31 13:31:27 +0100

    added color to our dependency

--- my-react-app/src/useEffects.jsx
+++ my-react-app/src/useEffects.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 
 function UseEffect() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("green");
 
   useEffect(() => {
-    document.title = `Count: ${count}`;
-  }, [count]);
+    document.title = `Count: ${count} ${color}`;
+  }, [count, color]);
 
   function add() {
     setCount((c) => c + 1);
@@ -13,11 +14,17 @@ function UseEffect() {
   function minus() {
     setCount((c) => c - 1);
   }
+
+  const changeColor = () => {
+    setColor((c) => (c === "green" ? "red" : "green"));
+  };
   return (
     <>
-      <h1>Count: {count}</h1>
+      <h1 style={{ color: color }}>Count: {count}</h1>
       <button onClick={add}>Add</button>
       <button onClick={minus}>Subtract</button>
+      <br />
+      <button onClick={changeColor}>Change Color</button>
       <br />
       <br />
     </>
