ba55636bccff6bba070f740df8c5c59084ecc9a4
Piwoksn <piwoksn@gmail.com>
2024-11-06 11:10:19 +0100

    done with the time. next to add zero to the seconds

--- my-react-app/src/DigitalClock.jsx
+++ my-react-app/src/DigitalClock.jsx
@@ -1,12 +1,34 @@
 import React, { useState, useEffect } from "react";
 
 function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  function formatTime() {
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridian = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+
+    return `${hours}:${minutes}:${seconds} ${meridian}`;
+  }
   return (
     <>
       <div className="clock-body">
         <div className="clock-container">
           <div className="clock">
-            <span>00:00:00</span>
+            <span>{formatTime()}</span>
           </div>
         </div>
       </div>
